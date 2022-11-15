851b642fb83f761d27718ec1b8cbb3e064e2c6cf
john <71388489+ooitanojohn@users.noreply.github.com>
2022-11-15 18:01:06 +0900

    Update index.php

--- acount/index.php
+++ acount/index.php
@@ -6,5 +6,13 @@ if ($_SERVER["REQUEST_METHOD"]) {
 if ($_GET['search']) {
 }
 // ブラウザからリクエストが来たら暫定 データを帰す場合jsonP形式にする
-$data = json_encode("object-key:"."object-val");
-echo "callback({" . $data . "})";
+$date = new DateTime();
+$data = [
+  1,
+  "string",
+  true,
+  [$date,null],
+  "name" => $_GET['name'],
+];
+$json = json_encode($data);
+echo "callback({" . $json . "})";
