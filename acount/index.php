cc09af2e2bc102eefca6d20f463338b829f9ddb6
john <71388489+ooitanojohn@users.noreply.github.com>
2022-11-15 17:40:47 +0900

    phpでのjsonPで帰す場合

--- acount/index.php
+++ acount/index.php
@@ -3,10 +3,8 @@
 // get or post
 if ($_SERVER["REQUEST_METHOD"]) {
 }
-if ($_GET['order']) {
-  // 並び替えクエリ発行
-  echo json_encode($data);
-}
-
 if ($_GET['search']) {
 }
+// ブラウザからリクエストが来たら暫定 データを帰す場合jsonP形式にする
+$data = json_encode($_GET['name']."購入者");
+echo "callback({" . $data . "})";
